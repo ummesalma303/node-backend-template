13dadb4ea8945aab10baeda6e30ea60be044c4e8
ummesalma303 <ummesalma333999@gmail.com>
2026-03-17 11:39:36 +0600

    feat: Setup expresss server with Typescript and npm scripts

--- dist/src/server.js
+++ dist/src/server.js
@@ -1,12 +1,10 @@
 import express from "express";
 const app = express();
 const port = "3000";
-
 app.get("/", (req, res) => {
-  res.send("Hello World!");
-  console.log("Response sent");
+    res.send("Hello World!");
+    console.log("Response sent");
 });
-
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+    console.log(`Example app listening on port ${port}`);
 });
