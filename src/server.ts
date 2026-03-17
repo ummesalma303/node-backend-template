4803bd8f75e12fd127e382aa8e91c89a12b2a00c
ummesalma303 <ummesalma333999@gmail.com>
2026-03-17 12:15:25 +0600

    chore: Setup ESLint, Prettier, and Vitest

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,10 @@
 import express from "express";
-const app = express();
-const port = "3000";
+import { middleware } from "#middlewares/middlewares.js";
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-  console.log("Response sent");
-});
+const app = express();
+const port = process.env.PORT ?? "9001";
+
+app.get("/", middleware);
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
